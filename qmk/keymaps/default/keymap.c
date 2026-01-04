c2e4a951ab74c137ed5bba74f2daa07bd89a98c4
cshapard26 <152207773+cshapard26@users.noreply.github.com>
2026-01-04 12:50:54 -0600

    Formatting

--- qmk/keymaps/default/keymap.c
+++ qmk/keymaps/default/keymap.c
@@ -7,7 +7,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-// Your LED pins
 #define RGB1_R_PIN GP8
 #define RGB1_B_PIN GP9
 #define RGB1_G_PIN GP10
@@ -16,7 +15,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define RGB2_B_PIN GP19
 #define RGB2_G_PIN GP20
 
-// Define static colors for the two LEDs (0=off, 1=on because common anode logic is inverted)
 #define LED1_RED    1
 #define LED1_GREEN  0
 #define LED1_BLUE   0
@@ -26,7 +24,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #define LED2_BLUE   0
 
 void keyboard_post_init_user(void) {
-    // Initialize GPIO pins as outputs
     gpio_set_pin_output(RGB1_R_PIN);
     gpio_set_pin_output(RGB1_G_PIN);
     gpio_set_pin_output(RGB1_B_PIN);
@@ -35,7 +32,6 @@ void keyboard_post_init_user(void) {
     gpio_set_pin_output(RGB2_G_PIN);
     gpio_set_pin_output(RGB2_B_PIN);
 
-    // Apply the static color (invert for common anode)
     gpio_write_pin(RGB1_R_PIN, !LED1_RED);
     gpio_write_pin(RGB1_G_PIN, !LED1_GREEN);
     gpio_write_pin(RGB1_B_PIN, !LED1_BLUE);
@@ -45,7 +41,6 @@ void keyboard_post_init_user(void) {
     gpio_write_pin(RGB2_B_PIN, !LED2_BLUE);
 }
 
-// If you still want to use led_update_kb, you can leave it empty or just pass through
 bool led_update_kb(led_t led_state) {
     led_update_user(led_state);
     return true;
